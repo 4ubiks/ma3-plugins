5a47f361a80c076db1f6c8453f8f7e23ac3d151f
4ubiks <jaharris20@liberty.edu>
2024-12-02 10:12:39 -0500

    fixed a few things with popup plugin, 'cancel' now works

--- popupMenu.lua
+++ popupMenu.lua
@@ -1,5 +1,8 @@
 local function main()
 
+	-- clears the programmer
+	Cmd("ClearAll")
+	
 	local states = {
 	{name = "HiLight", state=true, group=1},
 	{name = "Solo", state=true, group=2}
@@ -17,31 +20,33 @@ local function main()
 		MessageBox(
 		{
 			title= "Universe Selection",
-			message="Seect your universes, separated by comma, if multiple.",
+			message="Select your universes, separated by comma, if multiple.",
 			message_align_h = Enums.AlignmentH.Left,
 			message_align_v = Enums.AlignmentV.Top,
-			commands = {{value = 1, name="OK"}, {value=0, name="Cancel"}},
+			commands = {{value=1, name="Trace"}, {value=0, name="Cancel"}},
 			states=states,
 			inputs=inputs,
 			selectors=selectors,
 			backColor= "Global.Default",
 			icon="logo_small",
 			messageTextColor= "Global.Text",
-			autoCloseOnInput= true
+			autoCloseOnInput= false
 		}
 	)
-		
+	
+	
+	if resultTable.success then
+		if resultTable.result == 0 then
+			return
+		end
+	end
+	
+	
 	-- prints input values
 	for k,v in pairs(resultTable.inputs) do
 		--Printf("Input '%s' = '%s'", k,tostring(v))
 		inp = tostring(v)
-	end
-	
-	-- prints inputted values
-	--for k,v in pairs(resultTable.states) do
-		--Printf("State '%s' = '%s'", k,tostring(v))
-	--end
-	
+	end	
 	
 	-- hilight and solo toggle
 	for k,v in pairs(resultTable.states) do
@@ -68,7 +73,13 @@ local function main()
 	
 	
 	-- parse out commas from list, separate by whitespace
-	final = inp:gsub("%s*,%s*", " ")
+	fin = inp:gsub("%s*,%s*", " ")
+	local final = {}
+	
+	for num in fin:gmatch("%S+") do
+		table.insert(final, tonumber(num))
+	end
+	
 	
 	
 	-- Now, get to getting universe info
@@ -76,28 +87,26 @@ local function main()
 	local fixturesInPatch = GetSubfixtureCount()
 	
 	for i=1, #final do
-		if (final:sub(i, i) ~= " ") then
-			currentUniverse = tonumber(final:sub(i, i))
-			Printf("")
-			Printf("--------------------------------------------")
-			Printf("----------------Universe " .. currentUniverse .. "------------------")
+		currentUniverse = final[i]
+		Printf("")
+		Printf("--------------------------------------------")
+		Printf("----------------Universe " .. currentUniverse .. "------------------")
 			
-			myObjects = ObjectList("Fixture 1 thru ", fixturesInPatch)
-			countFix = 0
-			
-			for j=1,#myObjects do
-				uni = tonumber(string.match(myObjects[j].patch, "^%d+"))
-				if (uni == currentUniverse) then
-					countFix = countFix + 1
-					Printf("Address of Fixture " .. myObjects[j].fid .. " in universe " .. currentUniverse .. " = " .. myObjects[j].patch)
-					Cmd(myObjects[j].fid)
-				end
+		myObjects = ObjectList("Fixture 1 thru ", fixturesInPatch)
+		countFix = 0
+		
+		for j=1,#myObjects do
+			uni = tonumber(string.match(myObjects[j].patch, "^%d+"))
+			if (uni == currentUniverse) then
+				countFix = countFix + 1
+				Printf("Address of Fixture " .. myObjects[j].fid .. " in universe " .. currentUniverse .. " = " .. myObjects[j].patch)
+				Cmd(myObjects[j].fid)
 			end
-			
-			-- empty universe check
-			if (countFix == 0) then
-				ErrPrintf("Universe " .. currentUniverse .. " appears to be empty. Double check your patch, and try again. ")
-			end
+		end
+		
+		-- empty universe check
+		if (countFix == 0) then
+			ErrPrintf("Universe " .. currentUniverse .. " appears to be empty. Double check your patch, and try again. ")
 		end
 	end	
 end
