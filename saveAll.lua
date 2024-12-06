32c6463ed142fbd17b5870c7a5933c5e84b46132
4ubiks <144192094+4ubiks@users.noreply.github.com>
2024-12-06 08:26:40 -0500

    save all places

--- saveAll.lua
+++ saveAll.lua
@@ -1,8 +1,13 @@
 local function main()
-    local myObjects = ObjectList('Drive Thru')
-    for i = 1, #myObjects do
-        if (myObjects[i].drivetype ~= 'Internal' and myObjects[i].drivetype ~= 'Removable') then
-          Printf('Drive '..myObjects[i].no..' = '..myObjects[i].drivetype)
-        end
-    end
+  local myObjects = ObjectList('Drive Thru')
+  for i = 1, #myObjects do
+  	if (myObjects[i].drivetype == 'Internal' or myObjects[i].drivetype == 'Removeable') then
+  		local cD = myObjects[i].no
+  		Cmd("Select Drive " .. cD) 
+  		Cmd("SaveShow 'plugins'")
+    	  -- Printf('Drive '..myObjects[i].no..' = '..myObjects[i].drivetype)
+      end
   end
+end
+
+return main
