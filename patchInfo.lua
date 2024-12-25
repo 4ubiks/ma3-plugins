ab29bc86125e45256f70ff621bda2fa2b7d40521
4ubiks <144192094+4ubiks@users.noreply.github.com>
2024-12-24 22:11:50 -0700

    patchInfo changes, mostly works with manufacturer

--- patchInfo.lua
+++ patchInfo.lua
@@ -1,8 +1,29 @@
 local function print(table, x)
+	local myTypes = ObjectList("FixtureType 1 Thru")
+	local k = 1
+	
+	Printf(myTypes[2].manufacturer)
+	
+	for j=1, #myTypes do
+		if myTypes[j].name == x[k] then
+			local manu = tostring(myTypes[j].manufacturer)
+		end
+		k = k + 1
+	end
+	
+	manu = tostring(manu)
+	
 	for i=1, #x do
+		for j=1, #myTypes do
+			if tostring(myTypes[j].name) == tostring(x[i].fixturetype.name) then
+				manu = myTypes[j].manufacturer
+				manu = tostring(manu)
+			end
+		end
+		
 		Printf("========" .. x[i].fixture .. "========")
 		Printf("Fixture ID: " .. x[i].fixture)
-		Printf("Fixture Manufacturer: " .. x[i].name)
+		Printf("Fixture Manufacturer: " .. manu)
 		Printf("Fixture Type: " .. x[i].fixturetype.name)
 		Printf("Fixture mode: " .. x[i].mode)
 		Printf("Fixture address: " .. x[i].patch)
@@ -14,5 +35,7 @@ local function main()
 	local myObjects = ObjectList("Fixture 1 Thru")
 	print(table, myObjects)
 end
+
+
 
 return main
