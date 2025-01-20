6bce8e6d5ee629c6c2319ad3c4a147a51241ab55
4ubiks <jaharris20@liberty.edu>
2025-01-20 14:29:50 -0500

    updated fader ig

--- faderReturn.lua
+++ faderReturn.lua
@@ -1,3 +1,15 @@
 -- first draft: return what fader plugin is currently stored on
 -- second draft: return active value of fader (0, 50, 75, etc)
 -- final draft: change general universal value of RGB, CMY, or any other universal attribute for most fixtures
+
+-- currently prints out value of a hard-coded fader
+
+local function main()
+	local myObject = GetExecutor(201)
+	local myToken = 'FaderMaster'	
+	local val = myObject:GetFader{token=myToken}
+
+	Printf("your function is: " .. val)
+end
+
+return main
