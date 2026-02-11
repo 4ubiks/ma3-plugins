857de3dc92abb047acf069b61ab7f93637f9badf
4ubiks <jaharris20@liberty.edu>
2026-02-11 11:14:35 -0500

    altered message to be a bit more professional

--- src/letters/message.lua
+++ src/letters/message.lua
@@ -3,8 +3,8 @@ function message()
        local returnTable = MessageBox(
            {
                title = "Acknowledgement",
-               message = "Heads up, be sure you're referencing any\nmulti-instance fixtures by PARENT fixture ID. Also, be aware of \nflooding your command line. A fix is in the works!",
-               commands = {{value = 1, name = "Confirm"}}
+               message = "Be sure you're referencing any\nmulti-instance fixtures by parent fixture ID. The plugin will automatically\nassign values to respective subfixtures.",
+               commands = {{value = 1, name = "Got it"}}
            }
        )
    
