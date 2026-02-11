89f980e4797a4fb255f7143d9e19d362301723af
4ubiks <jaharris20@liberty.edu>
2026-02-11 12:24:40 -0500

    added first letter custom gui test file

---
+++ dev/faderGuiElement.lua
@@ -0,0 +1,5 @@
+-- Testing the creation of a Popup that integrates faders. 
+-- This is for the `letters/` plugin, but it's not a finished product. 
+-- Creating it here, s.t. I don't disrupt the working product. However, this belongs to the letters/ plugin. 
+
+
